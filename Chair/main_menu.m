adf3fce7d3aa4a095015234e51352f85552c7668
Killian Comerford <killian@Killians-MacBook-Pro.local>
2017-02-28 15:23:08 +0000

    main menu v2

--- Chair/main_menu.m
+++ Chair/main_menu.m
@@ -15,34 +15,30 @@
     [menu_view setBackgroundColor:[UIColor blueColor]];
     
     //background texture
-    UIImageView *background = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, view.frame.size.width, view.frame.size.height)];
-    menu_view.layer.anchorPoint = CGPointMake(.5, .5);
-    [background setImage:[UIImage imageNamed:@"background_menu"]];
+    UIImageView *background = [[UIImageView alloc]initWithFrame:CGRectMake(0,0, view.frame.size.width, view.frame.size.height)];
+    [background setImage:[UIImage imageNamed:@"menu_background"]];
+    
+    //title Texture
+    UIImageView *title = [[UIImageView alloc]initWithFrame:CGRectMake(menu_view.frame.size.width/2-(menu_view.frame.size.width/3),menu_view.frame.size.height/6,menu_view.frame.size.width/1.5,464*((menu_view.frame.size.width/1.5)/1910))];
+    [title setImage:[UIImage imageNamed:@"menu_title"]];
     
     //play button
     UIButton *play_button = [UIButton buttonWithType:UIButtonTypeCustom];
     [play_button addTarget:self action:@selector(play_button_action:) forControlEvents:UIControlEventTouchUpInside];
-    play_button.frame = CGRectMake((menu_view.frame.size.width/2)-((2*(menu_view.frame.size.width/3))/2),menu_view.frame.size.height/2-3.5*(169*(((2*(menu_view.frame.size.width/3))/1043))/2),2*(menu_view.frame.size.width/3),169*((2*(menu_view.frame.size.width/3))/1043));
-    [play_button setImage:[UIImage imageNamed:@"button_play"]forState:UIControlStateNormal];
+    play_button.frame = CGRectMake((2*(menu_view.frame.size.width/3))-(menu_view.frame.size.width/10),menu_view.frame.size.height/3,menu_view.frame.size.width/5,menu_view.frame.size.width/5);
+    [play_button setImage:[UIImage imageNamed:@"menu_button_play"]forState:UIControlStateNormal];
     //leaderboards button
     SEL aSel_leaderboards = @selector(leaderboards_button_action:);
     UIButton *leaderboards_button = [UIButton buttonWithType:UIButtonTypeCustom];
     [leaderboards_button addTarget:self action:aSel_leaderboards forControlEvents:UIControlEventTouchUpInside];
-    leaderboards_button.frame = CGRectMake((menu_view.frame.size.width/2)-((2*(menu_view.frame.size.width/3))/2),menu_view.frame.size.height/2-(169*(((2*(menu_view.frame.size.width/3))/1043))/2),2*(menu_view.frame.size.width/3),169*((2*(menu_view.frame.size.width/3))/1043));
-    [leaderboards_button setImage:[UIImage imageNamed:@"button_leaderboards"]forState:UIControlStateNormal];
-
-    //settings button
-    SEL aSel_settings = @selector(settings_button_action:);
-    UIButton *settings_button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [settings_button addTarget:self action:aSel_settings forControlEvents:UIControlEventTouchUpInside];
-    settings_button.frame = CGRectMake((menu_view.frame.size.width/2)-((2*(menu_view.frame.size.width/3))/2),menu_view.frame.size.height/2+1.5*(169*(((2*(menu_view.frame.size.width/3))/1043))/2),2*(menu_view.frame.size.width/3),169*((2*(menu_view.frame.size.width/3))/1043));
-    [settings_button setImage:[UIImage imageNamed:@"button_settings"]forState:UIControlStateNormal];
+    leaderboards_button.frame = CGRectMake(menu_view.frame.size.width/3-(menu_view.frame.size.width/10),menu_view.frame.size.height/3,menu_view.frame.size.width/5,menu_view.frame.size.width/5);
+    [leaderboards_button setImage:[UIImage imageNamed:@"menu_button_stats"]forState:UIControlStateNormal];
     
     [view addSubview:menu_view];
     [menu_view addSubview:background];
     [menu_view addSubview:play_button];
     [menu_view addSubview:leaderboards_button];
-    [menu_view addSubview:settings_button];
+    [menu_view addSubview:title];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
